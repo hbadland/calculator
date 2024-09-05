de7406aafaea1eecbe1232b118ce1c7b8eb2a35e
hbadland <badlandh@icloud.com>
2024-09-05 15:11:11 +0100

    Defined variables for each operator and num1+num2

--- calculator.js
+++ calculator.js
@@ -35,3 +35,20 @@ function divideNumbers (x, y) {
         return null;
     }
 }
+
+function operate(operator, num1, num2){
+    num1 = Number(num1);
+    num2 = Number(num2);
+    //switch statement as can be one of the 4 operations
+    switch(operator) {
+        case 'addNumbers':
+            return addNumbers(num1, num2);
+            break;
+        case 'subtractNumbers':
+            return subtractNumbers(num1, num2);
+            break;
+        case 'divideNumbers':
+            return divideNumbers(num1, num2);
+            break;
+    }
+}
